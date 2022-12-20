eb81aab1223d30dcfa297763853e3843c8ef71ad
Muhammad Abrar <mabrar000999@gmail.com>
2022-12-20 17:48:33 +0500

    UpdatedJS File

--- JS/script.js
+++ JS/script.js
@@ -9,3 +9,35 @@ toggleMenu.addEventListener("click", () => {
 closeMenu.addEventListener("click", () => {
   navMenu.classList.remove("show");
 });
+
+// < === Remove Menu After Click === >
+
+const navLink = document.querySelectorAll(".nav__link");
+function linkAction() {
+  navMenu.classList.remove("show");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+// < === Section Active Link === >
+
+const sections = document.querySelectorAll("sections[id]");
+window.addEventListener("scroll", scrollActive);
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    const sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active");
+    }
+  });
+}
